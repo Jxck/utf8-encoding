68c4be2f9ff04d78216b676e9afafb67576fdd92
Jxck <block.rxckin.beats@gmail.com>
2015-01-24 00:56:58 +0900

    add test file

--- utf8-encoding.ts
+++ utf8-encoding.ts
@@ -204,26 +204,4 @@ class Utf8Encoder implements Encoder {
   }
 }
 
-(function() {
-var encoder = new TextEncoder();
-
-[
-  [ "aAzZ09",    [ 97, 65, 122, 90, 48, 57 ]],
-  [ "~`!@",      [ 126, 96, 33, 64 ]],
-  [ "#$%^&",     [ 35, 36, 37, 94, 38 ]],
-  [ "*()_+-=",   [ 42, 40, 41, 95, 43, 45, 61 ]],
-  [ "{}|[]\:",   [ 123, 125, 124, 91, 93, 58 ]],
-  [ ";'<>?,./'", [ 59, 39, 60, 62, 63, 44, 46, 47, 39 ]],
-  [ "\"",        [ 34 ]],
-  [ "あ亞",      [ 227, 129, 130, 228, 186, 158 ]],
-  [ "叱𠮟",      [ 229, 143, 177, 240, 160, 174, 159 ]],
-  [ "🍻",          [ 240, 159, 141, 187 ]]
-].forEach(function(e) {
-  var s:string = <string>e[0];
-  var actual = encoder.encode(s);
-  var expected = e[1];
-  for (var i = 0; i < actual.length; i++) {
-    console.assert(actual[i] === expected[i]);
-  }
-});
-})();
+this.TextEncoder = TextEncoder;
