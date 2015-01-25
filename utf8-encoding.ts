8c8d6e659bfcda1a165cf25719375a2352e8715f
Jxck <block.rxckin.beats@gmail.com>
2015-01-26 01:13:14 +0900

    add fromCodePoint to String.fromCodePoint

--- utf8-encoding.ts
+++ utf8-encoding.ts
@@ -1,47 +1,75 @@
 /// <reference path="types/webidl.d.ts" />
 /// <reference path="types/obtain-unicode.d.ts" />
 
-// https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/String/fromCodePoint
-/*! http://mths.be/fromcodepoint v0.1.0 by @mathias */
-function fromCodePoint(codePoints: number[]): string {
-  var MAX_SIZE = 0x4000;
-  var codeUnits:number[] = [];
-  var highSurrogate: number;
-  var lowSurrogate: number;
-  var index = -1;
-  var length = codePoints.length;
-  if (!length) {
-    return '';
-  }
-  var result = '';
-  while (++index < length) {
-    var codePoint = Number(codePoints[index]);
-    if (
-      !isFinite(codePoint) ||       // `NaN`, `+Infinity`, or `-Infinity`
-      codePoint < 0 ||              // not a valid Unicode code point
-      codePoint > 0x10FFFF ||       // not a valid Unicode code point
-      Math.floor(codePoint) != codePoint // not an integer
-    ) {
-      throw RangeError('Invalid code point: ' + codePoint);
-    }
-    if (codePoint <= 0xFFFF) { // BMP code point
-      codeUnits.push(codePoint);
-    } else { // Astral code point; split in surrogate halves
-      // http://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
-      codePoint -= 0x10000;
-      highSurrogate = (codePoint >> 10) + 0xD800;
-      lowSurrogate = (codePoint % 0x400) + 0xDC00;
-      codeUnits.push(highSurrogate, lowSurrogate);
-    }
-    if (index + 1 == length || codeUnits.length > MAX_SIZE) {
-      result += String.fromCharCode.apply(null, codeUnits);
-      codeUnits.length = 0;
-    }
-  }
-  return result;
+/**
+ * Pollyfill of String.fromCodePoint
+ */
+declare var String: {
+  new (value?: any): String;
+  (value?: any): string;
+  prototype: String;
+  fromCharCode(...codes: number[]): string;
+  fromCodePoint(...codePoints: number[]): string;
 }
 
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/String/fromCodePoint
+/*! http://mths.be/fromcodepoint v0.1.0 by @mathias */
+if (!String.fromCodePoint) {
+  (function() {
+    var stringFromCharCode = String.fromCharCode;
+    var floor = Math.floor;
+    var fromCodePoint = function() {
+      var MAX_SIZE = 0x4000;
+      var codeUnits: number[] = [];
+      var highSurrogate: number;
+      var lowSurrogate: number;
+      var index = -1;
+      var length = arguments.length;
+      if (!length) {
+        return '';
+      }
+      var result = '';
+      while (++index < length) {
+        var codePoint = Number(arguments[index]);
+        if (
+          !isFinite(codePoint) ||       // `NaN`, `+Infinity`, or `-Infinity`
+          codePoint < 0 ||              // not a valid Unicode code point
+          codePoint > 0x10FFFF ||       // not a valid Unicode code point
+          floor(codePoint) != codePoint // not an integer
+        ) {
+          throw RangeError('Invalid code point: ' + codePoint);
+        }
+        if (codePoint <= 0xFFFF) { // BMP code point
+          codeUnits.push(codePoint);
+        } else { // Astral code point; split in surrogate halves
+          // http://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
+          codePoint -= 0x10000;
+          highSurrogate = (codePoint >> 10) + 0xD800;
+          lowSurrogate = (codePoint % 0x400) + 0xDC00;
+          codeUnits.push(highSurrogate, lowSurrogate);
+        }
+        if (index + 1 == length || codeUnits.length > MAX_SIZE) {
+          result += stringFromCharCode.apply(null, codeUnits);
+          codeUnits.length = 0;
+        }
+      }
+      return result;
+    };
+    if (Object.defineProperty) {
+      Object.defineProperty(String, 'fromCodePoint', {
+        'value': fromCodePoint,
+        'configurable': true,
+        'writable': true
+      });
+    } else {
+      String.fromCodePoint = fromCodePoint;
+    }
+  }());
+}
 
+/**
+ * Main
+ */
 import ObtainUnicode = require("obtain-unicode");
 
 type BufferSource = Uint8Array;
@@ -336,7 +364,7 @@ class TextDecoder implements ITextDecoder {
         // step 2-2-2
         else if (token !== EOS) {
           this.bomSeenFlag = true;
-          output += fromCodePoint([token]);
+          output += String.fromCodePoint(token);
         }
         // step 2-2-3
         else {
@@ -346,7 +374,7 @@ class TextDecoder implements ITextDecoder {
 
       // step 2-3
       else if (token !== EOS) {
-        output += fromCodePoint([token]);
+        output += String.fromCodePoint(token);
       }
 
       // step 2-4
